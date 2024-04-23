8298bc134cbf02c95da38dc7cbded65e305f3c49
alexandre <alexandrealvescs@gmail.com>
2024-04-23 20:42:58 -0300

    feat: directory for created environment control

---
+++ src/config/env/test.js
@@ -0,0 +1,14 @@
+class Config {
+    constructor () {
+      this.env = 'test'
+      this.PORT = process.env.PORT || 8000
+      this.API_BASE = '/api'
+      this.DATABASE_HOST = process.env.DATABASE_HOST || 'localhost'
+      this.DATABASE_PORT = 5432
+      this.DATABASE = process.env.DATABASE
+      this.DATABASE_USERNAME = process.env.DATABASE_USERNAME
+      this.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
+    }
+  }
+  
+module.exports = new Config()
