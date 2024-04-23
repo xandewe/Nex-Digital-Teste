21cc7442323f7ecfdec8416856d48a1cefc46adb
alexandre <alexandrealvescs@gmail.com>
2024-04-23 20:50:18 -0300

    refactor: USER entity adjustment

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,28 @@
-const { DataTypes } = require('sequelize');
-const db = require('../config/sequelize');
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      tableName: 'users'
+  });
 
-const User = db.define('User', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    tableName: 'users'
-});
+  return User
+};
   
-module.exports = User;
+module.exports = user;
