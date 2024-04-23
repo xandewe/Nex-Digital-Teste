27398708b2caf8d65997926c5c2cb2b99813ee7c
alexandre <alexandrealvescs@gmail.com>
2024-04-23 20:49:04 -0300

    refactor: argument adjustment for sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,11 +1,7 @@
 const Sequelize = require('sequelize')
+const dbConfig = require('./database')
 
-const user = process.env.DATABASE_USERNAME
-const password = process.env.DATABASE_PASSWORD
-const database = process.env.DATABASE
-const host = process.env.DATABASE_HOST
-
-const sequelize = new Sequelize((`postgres://${user}:${password}@${host}:5432/${database}`));
+const sequelize = new Sequelize(dbConfig);
 
 try {
     const conn = sequelize.sync();
