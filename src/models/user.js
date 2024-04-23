f4ac94ea81e6b17e75099b44124bf02f279cdee4
alexandre <alexandrealvescs@gmail.com>
2024-04-23 20:05:16 -0300

    feat: generate configs sequelize, and create model USER

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/sequelize');
+
+const User = db.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: 'users'
+});
+  
+module.exports = User;
