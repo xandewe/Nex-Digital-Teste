062868163adc3329bf207b49fa9743b06abe5a30
alexandre <alexandrealvescs@gmail.com>
2024-04-24 18:58:43 -0300

    feat: included middleware for products route GET

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const ProductController = require('../controllers/ProductController');
+const userMiddleware = require('../middleware/userMiddleware.js')
 
 const productController = new ProductController();
 
 const router = Router();
 
-router.get('/', (req, res) => productController.get(req, res));
 router.post('/', (req, res) => productController.post(req, res));
+
+router.use(userMiddleware.verifyToken);
+router.get('/', (req, res) => productController.get(req, res));
 
 module.exports = router;
