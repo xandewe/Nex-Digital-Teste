699313b93aa155b306d438ba93250887def95e15
alexandre <alexandrealvescs@gmail.com>
2024-04-24 17:08:57 -0300

    feat: added path /products in route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router()
 const usersRoutes = require('./userRoute.js');
+const productsRoutes = require('./productRoute.js');
 
 router.use('/users', usersRoutes)
+router.use('/products', productsRoutes)
 
 module.exports = router;
