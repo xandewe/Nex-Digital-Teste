814b4c559a035a3603445754fb616081d4fd7b65
alexandre <alexandrealvescs@gmail.com>
2024-04-24 00:03:52 -0300

    feat: using middleware to parse JSON and add API_BASE along with route registration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const config = require('./config')
+const routes = require('./routes')
 
 const app = express()
+app.use(express.json())
 
 app.get("/api/check", (req, res) => {
     res.json({success: true}), 200
 })
+
+app.use(config.API_BASE, routes)
 
 module.exports = app
