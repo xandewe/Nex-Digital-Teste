6101dd933769892103e5a59f885809fc619d0b0b
alexandre <alexandrealvescs@gmail.com>
2024-04-24 18:57:43 -0300

    feat: middleware created for verification of token

---
+++ src/middleware/userMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+function verifyToken (req, res, next) {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+      return res.status(401).json({msg: 'No Token provided'});
+    }
+  
+    jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(500).json({msg: 'Failed to authenticate token!'});
+      }
+      req.userId = decoded.id;
+      next();
+    })
+  }
+  
+  module.exports = { verifyToken }
