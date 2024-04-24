c588dd1da21ccd5eb8b7442acc1e03640e327538
alexandre <alexandrealvescs@gmail.com>
2024-04-24 18:17:33 -0300

    feat: create signin and genToken services

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,8 +1,35 @@
 const Services = require('./Services.js');
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+const bcrypt = require('bcrypt');
+
+const { compare } = bcrypt
 
 class UserServices extends Services {
     constructor () {
         super('User');
+    }
+
+    genToken(user) {
+        const token = jwt.sign({ id: user.id }, config.JWT_SECRET, { expiresIn: '1d' })
+        return token
+    }
+
+    async signin(email, password) {
+        const user = this.getBy({ email })
+
+        if(user === null){
+            throw new Error('Login invalid')
+        }
+
+        const passIsValid = compare(password, user.password)
+        if (!passIsValid) {
+            throw new Error('Login invalid')
+          }
+
+        const token = this.genToken(user)
+        const { name } = user
+        return { token, data: { name, email }};
     }
 }
 
