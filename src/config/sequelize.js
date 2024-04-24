4d9e3481735d653798dea84883e5c97ab2fe4726
alexandre <alexandrealvescs@gmail.com>
2024-04-23 21:15:12 -0300

    refactor: remove sync sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,10 +3,4 @@ const dbConfig = require('./database')
 
 const sequelize = new Sequelize(dbConfig);
 
-try {
-    const conn = sequelize.sync();
-} catch(error) {
-    console.log(error);
-}
-
 module.exports = sequelize;
