cba809c3c9bbce44faea98119deb1ea14d7de8c4
alexandre <alexandrealvescs@gmail.com>
2024-04-24 16:20:50 -0300

    feat: added response errors

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -5,20 +5,20 @@ class Controller {
   
     async get(req, res) {
       try {
-        const list_all = await this.entityService.pegaTodosOsRegistros();
+        const list_all = await this.entityService.get_all();
         return res.status(200).json(list_all);
       } catch (erro) {
-        // erro
+        res.status(400).json({error: err.message})
       }
     }
   
     async post(req, res) {
       const payload = req.body;
       try {
-        const create_new = await this.entityService.criaRegistro(payload);
+        const create_new = await this.entityService.create(payload);
         return res.status(201).json(create_new);
       } catch (erro) {
-        // erro
+        res.status(400).json({error: err.message})
       }
     }
 
