a29e82c46cc72e959b44d0773576fc4f5c332bfe
alexandre <alexandrealvescs@gmail.com>
2024-04-23 21:15:38 -0300

    clean code

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,5 @@
 const config = require('./index')
 
-console.log('#######database########')
-console.log(config)
-console.log('###############')
-
 module.exports = {
     dialect: 'postgres',
     username: config.DATABASE_USERNAME,
