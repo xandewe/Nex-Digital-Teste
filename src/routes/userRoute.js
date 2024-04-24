1a964574c150c3702268cc0b74c1897f5ccb229b
alexandre <alexandrealvescs@gmail.com>
2024-04-24 17:07:45 -0300

    refactor: remove method GET of userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,7 +5,6 @@ const userController = new UserController();
 
 const router = Router();
 
-router.get('/', (req, res) => userController.get(req, res));
 router.post('/', (req, res) => userController.post(req, res));
 
 module.exports = router;
