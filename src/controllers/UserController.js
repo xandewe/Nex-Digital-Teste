28945c5b5e34a98ca05c43cd1e554bae082f27cc
alexandre <alexandrealvescs@gmail.com>
2024-04-24 17:47:16 -0300

    feat: overriding post method in UserController by adding hash to password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,32 @@
 const Controller = require('./Controller.js');
 const UserServices = require('../services/UserServices.js');
+const bcrypt = require('bcrypt');
 
+const saltRounds = 12;
+const { hash } = bcrypt
 const userServices = new UserServices();
 
 class UserController extends Controller {
   constructor() {
     super(userServices);
+  }
+
+  async post(req, res) {
+    const payload = req.body;
+    try {
+      const pass = payload.password
+
+      const passhash = await hash(pass, saltRounds)
+      payload.password = passhash
+      const create_new = await this.entityService.create(payload);
+
+      delete payload['password']
+
+      return res.status(201).json({"id": create_new.id, ...payload});
+
+    } catch (err) {
+      res.status(400).json({error: err.message})
+    }
   }
 }
 
