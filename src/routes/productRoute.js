98ae6624fafbd0f63b5393e779bfec77e1d1d1eb
alexandre <alexandrealvescs@gmail.com>
2024-04-25 12:40:07 -0300

    feat: added swagger doc for POST products

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -6,6 +6,30 @@ const productController = new ProductController();
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/productss:
+ *   post:
+ *     summary: Retorna todos os produtos
+ *     description: Obtém uma lista de todos os produtos cadastrados.
+ *     responses:
+ *       200:
+ *         {
+ *           "docs": [
+ *              {
+ *                 "id": UUID,
+ *                 "name": STR,
+ *                 "value": STR,
+ *                 "valuePer": STR,
+ *                 "createdAt": TIMESTAMP,
+ *                 "deletedAt": TIMESTAMP,
+ *                 "updatedAt": TIMESTAMP
+ *              }
+ *           ],
+ *           "pages": INT,
+ *           "total": INT
+ *          }
+ */
 router.post('/', (req, res) => productController.post(req, res));
 
 router.use(userMiddleware.verifyToken);
